f2dd2c1613a58b0447a8b9fc6c870c98136f58f4
allenz3 <allenz3@uw.edu>
2022-01-24 01:50:53 -0800

    colors changed, image changed, comments added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
 function App() {
+  // one header container with the logo and the title, and one footer container with the user text input and the send button
   return (
   <div className="App">
     <header className="header">
       <div className="logo" />
-      <span className="title">CHATTER!</span>
+      <span className="title">CHATTER</span>
     </header>
     <footer className="footer">
       <input className="text-input" />
-      <button className="send">➞</button>
+      <button className="send-button">➞</button>
     </footer>
   </div>
   );
@@ -19,5 +20,10 @@ export default App;
 
 /* Sources:
 https://github.com/Evanfeenstra/chatter_2022
-
+https://www.w3schools.com/css/css_background.asp
+https://www.w3schools.com/CSSref/pr_background-color.asp
+https://www.w3schools.com/csSref/css3_pr_background-size.asp
+https://unicode-table.com/en/279E/
+https://www.createdebate.com/debate/show/Oklahoma_City_Most_Normal_City_in_America
+https://coolors.co/aa90dd-ab9edd-aebadd-b1d6dd-b3f2dd
 */
